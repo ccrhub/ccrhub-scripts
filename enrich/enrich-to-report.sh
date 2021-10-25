684e69e2f39232c17e62dc157acc3c6754e00935
gbreavin <gary.breavington@gmail.com>
2021-10-25 21:56:10 +1100

    Amending URL
    "endpoints" missing from URL

--- enrich/enrich-to-report.sh
+++ enrich/enrich-to-report.sh
@@ -22,7 +22,7 @@ while getopts "h:e:k:r:f:" arg; do
 done
 
 
-jobId=`curl "$host"'/.netlify/functions/job-starter' \
+jobId=`curl "$host"'/.netlify/functions/endpoints/job-starter' \
   -H 'Content-Type: application/json' \
   -H 'X-API-KEY: '"$apikey" \
   --data '{"functionId":"enrich-to-report-background","reportId":"'"$reportId"'","enricherId":"'"$enricherId"'"}' \
